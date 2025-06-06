2ca64496527c501fc4a7225c065975c626b737df
mrwilliamyuan <jurassictortoises@gmail.com>
2025-06-06 13:41:12 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,12 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "williamyuan"         # 用户名
+
+UUID = "bf917132-719f-41f1-aca8-684c6f69e8fa"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "williamyuan.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "cloudflared.exe service install eyJhIjoiNTY4OGNkM2E4YjFkMTc4OTQyZmQ0ODZjZTllNDk0MzAiLCJ0IjoiZTRlMzE5ZTgtOWRjZi00NGRjLTgxNjAtNmI0MmIxYjkxYzgwIiwicyI6IlkyRTFOelZsTVRZdE9XSXlOaTAwTnpFMkxXSXdaV1F0Wm1ZME1tWXhPV1JrWVRjeiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
